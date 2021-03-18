e1eb0c859fbef8d5ba045e019d7c04906baccfd4
tomppk <tomi.kivioja@gmail.com>
2021-03-18 09:34:51 +0200

    class inheritance example

--- classes.ts
+++ classes.ts
@@ -6,12 +6,21 @@
 // and by child classes that extend this class
 
 class Vehicle {
+  constructor(public color: string) {}
+
   protected honk(): void {
     console.log('beep');
   }
 }
 
+const vehicle = new Vehicle('orange');
+console.log(vehicle.color);
+
 class Car extends Vehicle {
+  constructor(public wheels: number, color: string) {
+    super(color);
+  }
+
   private drive(): void {
     console.log('chugga chugga');
   }
@@ -22,6 +31,6 @@ class Car extends Vehicle {
   }
 }
 
-const car = new Car();
+const car = new Car(4, 'blue');
 car.startDriving();
-car.honk();
+console.log(car.color);
